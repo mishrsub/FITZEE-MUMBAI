8403603bf3fce6b269c9e0b04922fad287f6ccd3
mishrsub <mishrsub@gmail.com>
2024-03-29 18:35:43 +0530

    giving port number 80

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+    // import dotenv from "dotenv";
+    // dotenv.config();
 import "dotenv/config.js"
 import { app } from "./app.js";
 import { connectDB } from "./db/conn.js";
-const PORT = process.env.PORT || 8001;
+const PORT = 80 || process.env.PORT || 8001;
 
 app.listen(PORT, async () => {
     console.log(`server is listening to the port ${PORT}`);
